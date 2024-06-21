677f74659e1888ba9011901fb4a3b47f5a87a2a9
tangkasR <tangkasrisdianto2@gmail.com>
2024-06-21 23:11:43 +0700

    initial push

--- src/routes/predict.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import predictDisease from '../controllers/predictController.js';
-
-const router = express.Router();
-
-router.post('/predict', predictDisease);
-
-export default router;
